871ada82051c2c57e1426ca6e6c48d14c3c0e48d
Mohamed Zakaria <m.zakaria.elzoghbi@gmail.com>
2018-06-30 19:00:01 +0200

    Adding circle ci 2 yml file

--- zgallery/src/test/java/com/mzelzoghbi/zgallery/ExampleUnitTest.java
+++ zgallery/src/test/java/com/mzelzoghbi/zgallery/ExampleUnitTest.java
@@ -12,4 +12,12 @@ public class ExampleUnitTest {
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
     }
+    @Test
+    public void addition_isCorrect1() throws Exception {
+        assertEquals(5, 3 + 2);
+    }
+    @Test
+    public void addition_isCorrect2() throws Exception {
+        assertEquals(6, 3 + 3);
+    }
 }
